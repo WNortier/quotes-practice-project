857ec56af1bbdc93106649f78ae017538a5b9003
wnortier <warwick.nortier@gmail.com>
2021-10-14 15:28:43 +0200

    added a not found page

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -2,7 +2,7 @@ import QuoteList from "../components/quotes/QuoteList";
 
 const DUMMY_QUOTES = [
   { id: "q1", author: "Warwick", text: "Learning React is fun" },
-  { id: "q1", author: "Leon", text: "Learning React is fun" },
+  { id: "q2", author: "Leon", text: "Learning React is fun" },
 ];
 
 const AllQuotes = () => {
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,26 @@
 import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import QuoteItem from "../components/quotes/QuoteItem";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Warwick", text: "Learning React is fun" },
+  { id: "q2", author: "Leon", text: "Learning React is fun" },
+];
+
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quotes Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments></Comments>
       </Route>
