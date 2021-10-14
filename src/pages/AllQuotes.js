7cd4695c7b872040a223c5fa181cf753dc06c7fe
wnortier <warwick.nortier@gmail.com>
2021-10-14 14:30:55 +0200

    adding dummy data & more content

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Warwick", text: "Learning React is fun" },
+  { id: "q1", author: "Leon", text: "Learning React is fun" },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES}></QuoteList>;
 };
 
 export default AllQuotes;
